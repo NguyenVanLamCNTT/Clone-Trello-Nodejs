a1c4f1897e0362d6303a17861d10bf90dfa90b22
Nguyễn Văn Lâm <ngocthau047@gmail.com>
2021-09-04 15:37:43 +0700

    init project

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get('/',(req,res) => {
+    res.send('Clone Trello');
+});
+
+//Connect mongodb
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(()=> console.log('Mongodb connected'))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,() =>{
+    console.log(`http://localhost:${PORT}`);
+})
